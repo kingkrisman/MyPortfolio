32defdcc40206ded8d85efde073226a1a7f3f062
kingkrisman <kingkrisman@gmail.com>
2024-11-06 20:40:21 -1100

    edit

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -34,23 +34,21 @@ const Portfolio=()=>{
           <div className="work">
             <img src={worktwo} />
             <div className="layer">
-              <h3>Kuda Clone</h3>
+              <h3>ProDoc(AI)</h3>
               <p>
-              Working on a Kuda Clone.
+            ProDoc is a web app designed specifically for technical writers. It main purpose is to simplify the process of creating, formatting, and publishing technical Doc...
               </p>
 
-              <a href="https://kuda-teal.vercel.app/">↗</a>
+              <a href="https://artificial-intelligence-y7y4-dn3vp0tfv.vercel.app">↗</a>
             </div>
           </div>
           <div className="work">
             <img src={workthree} />
             <div className="layer">
-              <h3> Food delivary App</h3>
-              <p>
-                Design of a Food delivary Mobile app for a startup
-                company.
-              </p>
-              <a href="https://www.figma.com/design/rTAL74rGqvbjszEAD5KBzR/Untitled?node-id=82-227&t=goRo4Mv6SQSyO8EI-1">↗</a>
+              <h3>Weather App</h3>
+              <p> App that displays the current weather of a given location.</p>
+
+              <a href="https://kingkrisman.github.io/weather_app/">↗</a>
             </div>
           </div>
         </div>
